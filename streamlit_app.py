843cf1a01321286f4d524e3585fa028ee6273caf
SnekaSelvam217 <uniquesneha217@gmail.com>
2026-01-14 13:59:08 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+Filter food listings by city, food type, provider.
+
+Display top providers, top receivers.
+
+Allow CRUD operations (add/update/delete listings & claims).
+
+Show simple charts: top providers, claims by meal type, food availability per city.
